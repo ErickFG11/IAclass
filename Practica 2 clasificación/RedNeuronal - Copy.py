746b1ef22c496483bfa6f8125c0a41898068ae26
Erick Franco <e.francogaona@ugto.mx>
2022-11-22 14:28:29 -0600

    Optimizacion practica 2

--- Practica 2 clasificación/RedNeuronal - Copy.py
+++ Practica 2 clasificación/RedNeuronal - Copy.py
@@ -1,20 +1,36 @@
-from keras.layers import Dense, Dropout
+from keras.layers import Dense, Dropout, Flatten
 from keras.models import Sequential
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import keras.utils as ku
-import numpy as np
-from sklearn.metrics import accuracy_score, confusion_matrix
 import keras_tuner as kt
+from keras import optimizers
 
-def modelo(hp):
+def construir_modelo(hp):
+    n_hidden=hp.Int("n_hidden", min_value=0, max_value=8, default=2)
+    n_neurons=hp.Int("n_neurons", min_value=16, max_value=256)
+    learning_rate=hp.Float("learning_rate", min_value=1e-4, max_value=1e-2, sampling="log")
+    optimizer=hp.Choice("optimizer", values=["sgd", "adam"])
+    if optimizer=="sgd":
+        optimizer=optimizers.SGD(learning_rate=learning_rate)
+    else:
+        optimizer=optimizers.Adam(learning_rate=learning_rate)
 
+    model=Sequential()
+    model.add(Flatten())
+    for _ in range(n_hidden):
+        model.add(Dense(n_neurons, activation='relu'))
+    model.add(Dense(7, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+    return model
 
 if __name__ == '__main__':
     names = ['BARBUNYA','BOMBAY','CALI','DERMASON','HOROZ','SEKER','SIRA']
     numeroclases=7
+    n_hidden=7
+    n_neurons=124
     #importar dataset
     dataset=pd.read_csv("Dry_Bean_Dataset.csv")
     print('Dataset Original \n',dataset)
@@ -44,27 +60,16 @@ if __name__ == '__main__':
     X_train, X_test, Y_train, Y_test=train_test_split(X,Y,test_size=0.2, random_state=0)
     #modelo de red
     model=Sequential()
-    model.add(Dense(units=32, activation='relu'))
-    model.add(Dense(units=16, activation='relu'))
-    model.add(Dense(units=8, activation='relu'))
+    model.add(Flatten())
+    for _ in range(n_hidden):
+        model.add(Dense(n_neurons, activation='relu'))
     model.add(Dense(units=numeroclases, activation='softmax'))
     #compilar modelo
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    random_search_tuner=kt.RandomSearch(construir_modelo, objective="val_accuracy", max_trials=20, seed=42,
+                                    overwrite=True, project_name="frijoles")
     #ejecutar red neuronal
-    model.fit(X_train, Y_train, epochs=100, batch_size=32)
+    random_search_tuner.search(X_train, Y_train, epochs=20, validation_data=(X_test, Y_test))
 
-    #Prediccion
-    y_pred = model.predict(X_test)
-    predicciones=[]
-    reales=[]
-    for i in range(len(y_pred)):
-        predicciones.append(names[np.argmax(y_pred[i])])
-        reales.append(names[np.argmax(Y_test[i])])
-
-    accu=accuracy_score(reales, predicciones)
-    print("Model Accuracy: {:.2f}%".format(accu * 100))
-    #matriz de confusion
-    cm=confusion_matrix(reales, predicciones)
-    print(cm)
-
+    best_trial=random_search_tuner.get_best_hyperparameters(num_trials=3)
+    print(best_trial[0].values)
 
